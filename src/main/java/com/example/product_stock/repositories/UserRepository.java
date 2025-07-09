2f619b30949f73a88a215dba99c7f7991ce53622
Marcelo <cordolla10@gmail.com>
2025-07-09 13:04:37 -0300

    feat(user): Prevent creation of duplicate users
    Before saving a new user, check if another user already exists
    with the same username or email.
    
    If a duplicate is found, a `UserFoundException` is thrown,
    preventing a DataIntegrityViolationException from the database
    and providing a clearer API response (409 Conflict).

--- src/main/java/com/example/product_stock/repositories/UserRepository.java
+++ src/main/java/com/example/product_stock/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.product_stock.repositories;
 import com.example.product_stock.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsernameOrEmail(String uusername, String email);
 }
--- src/main/java/com/example/product_stock/services/UserService.java
+++ src/main/java/com/example/product_stock/services/UserService.java
@@ -3,6 +3,7 @@ package com.example.product_stock.services;
 import com.example.product_stock.dtos.UserRequestDTO;
 import com.example.product_stock.dtos.UserResponseDTO;
 import com.example.product_stock.entities.User;
+import com.example.product_stock.exceptions.UserFoundException;
 import com.example.product_stock.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -23,14 +24,20 @@ public class UserService {
     }
 
     @Transactional
-    public UserResponseDTO createUser(UserRequestDTO requestDTO){
+    public UserResponseDTO createUser(UserRequestDTO requestDTO) {
+        if (userRepository.findByUsernameOrEmail(requestDTO.getUsername(), requestDTO.getEmail()).isPresent()){
+            throw new UserFoundException();
+        }
+
         User user = modelMapper.map(requestDTO, User.class);
         user.setId(null);
         User savedUser = userRepository.save(user);
+
         return modelMapper.map(savedUser, UserResponseDTO.class);
+
     }
 
-    public List<UserResponseDTO> findAllUserResponseDTO(){
+    public List<UserResponseDTO> findAllUserResponseDTO() {
         return userRepository.findAll().stream()
                 .map(user -> modelMapper.map(user, UserResponseDTO.class))
                 .collect(Collectors.toList());
